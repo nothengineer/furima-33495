f920bfb7289cd3ef8cdb10373fc521f283d90d25
nothengineer <kiwami.group.yuya@gmail.com>
2021-01-20 13:16:47 +0900

    商品情報編集機能　レビュー後修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:edit,:destroy]
-
+  before_action :set_item, only:[:show,:edit,:update]
 
   def index
     @items = Item.all.order(id: "DESC")
@@ -46,5 +46,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :text, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 
-
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
