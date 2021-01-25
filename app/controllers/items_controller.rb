2acf4728ec8924cd925808a2b2dc11233b6a2979
nothengineer <kiwami.group.yuya@gmail.com>
2021-01-25 12:54:31 +0900

    商品購入機能実装　レビュー後修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless @item.user_id == current_user.id
+    if @item.user_id == current_user.id || @item.order.present?
       redirect_to root_path
     end
   end
