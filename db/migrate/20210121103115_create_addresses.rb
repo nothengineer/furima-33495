22a8d644a50533e89729f3654e9cf7272236d90b
nothengineer <kiwami.group.yuya@gmail.com>
2021-01-25 15:46:41 +0900

    商品購入機能（本番環境エラー解消）

--- db/migrate/20210121103115_create_addresses.rb
+++ db/migrate/20210121103115_create_addresses.rb
@@ -5,6 +5,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.integer      :prefecture_id,      null: false
       t.string       :municipalities,     null: false
       t.string       :address,            null: false
+      t.string       :building_name
       t.string       :phone_number,       null: false
       t.references   :order,              foreign_key: true
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_01_21_103115) do
     t.integer "prefecture_id", null: false
     t.string "municipalities", null: false
     t.string "address", null: false
+    t.string "building_name"
     t.string "phone_number", null: false
     t.bigint "order_id"
     t.datetime "created_at", precision: 6, null: false
